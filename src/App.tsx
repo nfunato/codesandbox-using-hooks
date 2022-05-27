7442737100f4183dd6cb4ec31bd6f0638317715e
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 22:48:24 +0900

    add data acquisition function w/o hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import type { User } from "./types/api/user";
+import { useState } from "react";
+import type { UserProfile } from "./types/UserProfile";
+
+/*
+const user = {
+  id: 1,
+  name: "わしじゃ",
+  email: "12132aaa.com",
+  address: "東京湾"
+};
+*/
+
+const style = {
+  border: "solid 1px #ccc",
+  borderRadius: "8px",
+  padding: "0 16px",
+  margin: "8px"
+};
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const onClickFetchData = () => {
+    setLoading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App" style={style}>
+      <button onClick={onClickFetchData}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
