91a390a12444914264127f524bce9219eca38bb2
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-27 23:23:21 +0900

    add a custom hook see comment for App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import type { User } from "./types/api/user";
-import { useState } from "react";
-import type { UserProfile } from "./types/UserProfile";
-
-/*
-const user = {
-  id: 1,
-  name: "わしじゃ",
-  email: "12132aaa.com",
-  address: "東京湾"
-};
-*/
+import { useAllUsers } from "./hooks/useAllUsers";
 
 const style = {
   border: "solid 1px #ccc",
@@ -22,30 +10,15 @@ const style = {
 };
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  // useAllUsersフックを他のcallerから使った場合、
+  // 返り値の状態 userProfiles/loading等は、そのcallerに固有のものとなる
+  // ことが重要と考えられる
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchData = () => getUsers();
+
+  // Loading... の表示中が遷移中に現れて消えるロジックは
+  // 下記コードでは自明ではないように思われる
   return (
     <div className="App" style={style}>
       <button onClick={onClickFetchData}>データ取得</button>
